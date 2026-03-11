f6dd36971bc1026bebe8513fb3c8f245ff1dcb7a
turolin <kt01098702642@gmail.com>
2026-03-11 10:14:09 +0900

    2026-03-11

--- src/secu/guiTest_swing.java
+++ src/secu/guiTest_swing.java
@@ -3,9 +3,15 @@ package secu;
 import javax.swing.*;
 
 public class guiTest_swing extends JFrame {
+    //창 사이즈
+    //창 표시여부
+    //여러 기본설정
+    //패널도 여기서 추가
 
-}
+    class gui_panel extends JPanel{
+        //실제 창 안의 값
 
-class gui_panel extends JPanel{
-
+        // 상태창,미로,숫자 표시 등등
+        //값을 넣고 프레임에 넣기
+    }
 }
